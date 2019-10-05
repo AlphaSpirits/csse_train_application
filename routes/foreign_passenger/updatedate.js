8b9c1f3f451569f820650e440347a9bca0a3269d
AlphaSpirits <alpha.4spirits@gmail.com>
2019-10-05 08:13:10 +0530

    nineth commit for passenger

--- routes/foreign_passenger/updatedate.js
+++ routes/foreign_passenger/updatedate.js
@@ -6,49 +6,6 @@ const constants=require('../../constants');
 var moment = require("moment");
 var momentt = require("moment-timezone");
 
-//router.post("/", function(req, res, next) {
-    //For stats
-  //expireDate
-    //console.log(req.body.expireDate);
-    // var fomatted_journeydate = momentt(req.body.journeryDate)
-    //   .tz("GMT")
-    //   .format("YYYY-DD-MM");
-  
-    // var currentdate = new Date();
-  
-    // var fomatted_currentdate = moment(currentdate.toString())
-    //   .tz("GMT")
-    //   .format("YYYY-DD-MM");
-  
-    // var day = momentt(req.body.journeryDate)
-    //   .tz("GMT")
-    //   .format("ddd");
-    // console.log(day);
-    // var hour = momentt(req.body.journeryDate)
-    //   .tz("GMT")
-    //   .format("H");
-  
-    // console.log(hour);
-    // console.log(fomatted_journeydate);
-    // console.log(fomatted_currentdate);
-  
-    // //Token Validity check
-  
-  
-    // if (fomatted_journeydate == fomatted_currentdate) {
-    //   ValidJourneydetails.create(req.body)
-    //     .then(function(validjourneydetails) {
-    //       res.send(validjourneydetails);
-    //     })
-    //     .catch(next);
-    // } else {
-    //   InValidJourneydetails.create(req.body)
-    //     .then(function(invalidjourneydetails) {
-    //       res.send(invalidjourneydetails);
-    //     })
-    //     .catch(next);
-    // }
- // });
 var fineamountt=5555;
  router.patch('/:refUserId',(req,res,next)=>{
     const refUserId = req.params.refUserId;
@@ -64,32 +21,6 @@ var fineamountt=5555;
          error:err
        });
     });
-
-
-//   router.put("/id/:userid/:date", function(req, res, next) {
-//     //   console.log(req.params.userid);
-//     //   console.log(req.params.date);
-//     //   var day=req.params.date;
-
-//     //   var dd=moment(day).subtract('months', 1);
-//     //  // const formatted = moment(dd).format('yyyyMMdd');
-//     //  const formatted=new moment(dd,'yyyyMMddHHmmssfff').toDate()
-//     //   console.log("day "+day);
-//     //   console.log("moment "+dd);
-//     //   console.log("formatted "+formatted);
-   
-      
-//      User.findByIdAndUpdate({ _id: req.params.userid }, req.body).then(
-//        function() {
-//        User.findOne({ refUserId: req.params.userid })
-//            .then(function(user) {
-//              res.send(user);
-//           })
-//            .catch(next);
-//        }
-//      );
-    
-//   });
 
 router.put("/id/:userid/:date", function(req, res, next) {
     console.log("date "+req.params.date);
